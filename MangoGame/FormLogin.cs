8099e35f0ad2c37ec6aebe4c42fd1ee5671de4af
banagan <snowboy845@live.com>
2014-04-26 16:14:25 +0800

    gan的测试恢复
    恢复原状态，删除无意义输入。

--- MangoGame/FormLogin.cs
+++ MangoGame/FormLogin.cs
@@ -14,7 +14,6 @@ namespace MangoGame
         public FormLogin()
         {
             InitializeComponent();
-            //
             // 如果勾选"记住密码"
             if(chkPassword.Checked)
             {
