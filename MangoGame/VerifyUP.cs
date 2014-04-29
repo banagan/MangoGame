b890c46000a128502fb70d9736bfcd610a042b35
idreamshen <idream.shen@gmail.com>
2014-04-29 15:40:29 +0800

    更改VerifyUP验证
    注册与登陆的本地验证合并

--- MangoGame/VerifyUP.cs
+++ MangoGame/VerifyUP.cs
@@ -8,17 +8,38 @@ namespace MangoGame
 {
     class VerifyUP
     {
+        public VerifyUP(string username, string password1, string password2)
+        {
+            errorVerUsername = VerUsername(username);
+            errorVerPassword = VerPassword(password1, password2);
+        }
+
+        public int errorVerUsername
+        {
+            set;
+            get;
+        }
+
+        public int errorVerPassword
+        {
+            set;
+            get;
+        }
+
         // 验证账号枚举
         // success/0 = 验证成功; invalidCharacter/1 = 包含非法字符; invalidLength/2 = 长度不满足;
+        /*
         public enum errorVerUsername : int
         {
             success = 0,
             invalidCharacter = 1,
             invalidLength = 2,
         }
+        */
 
         // 验证密码枚举
         // success/0 = 验证成功; invalidCharacter/1 = 包含非法字符; invalidLength/2 = 长度不满足; mismatch/3 = 重复密码不匹配
+        /*
         public enum errorVerPassword : int
         {
             success = 0,
@@ -26,9 +47,10 @@ namespace MangoGame
             invalidLength = 2,
             mismatch = 3,
         }
+        */
 
         // 验证账号函数
-        public int VerUsername(string username)
+        private int VerUsername(string username)
         {
             Regex usernameregex = new Regex("^[a-zA-Z0-9\u4e00-\u9fa5]+$");
             Match m = usernameregex.Match(username);
@@ -48,11 +70,16 @@ namespace MangoGame
         }
 
         // 验证密码函数
-        public int VerPassword(string password)
+        private int VerPassword(string password1, string password2)
         {
             Regex passwordregex = new Regex("^[a-zA-Z0-9]+$");
-            Match m = passwordregex.Match(password);
-            if (password.Length>10||password.Length<3)
+            Match m = passwordregex.Match(password1);
+
+            if(password1 != password2)
+            {
+                return 3;
+            }
+            else if (password1.Length > 10 || password1.Length < 3)
             {
                 return 2;
             }
@@ -64,21 +91,8 @@ namespace MangoGame
             {
                 return 1;
             }
-        }
 
-        // 验证重复密码函数
-        // 0 = 验证成功; 1 = 验证失败
-        public int VerPasswordRepeat(string password1, string password2)
-        {
-            if (password1 == password2)
-            {
-                return 0;
-            }
-            else
-            {
-                return 1;
-            }
-                
+
         }
     }
 }
